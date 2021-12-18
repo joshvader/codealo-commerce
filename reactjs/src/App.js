f8e80e07f224f2f00f206fce7b31f5a79ff51221
proyect_icaro <josuevalenzuela87@gmail.com>
2021-12-18 11:07:16 -0300

    Limpieza de archivos y creación de la sección Header

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/sections/Header";
+import "./styles/styles.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edita <code>src/App.js</code> y guarda para ver cambios.
-        </p>
-        <a
-          className="App-link"
-          href="https://codealo.dev"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Aprende con Codealo
-        </a>
-      </header>
+     <Header/>
     </div>
   );
 }
---
+++ reactjs/src/components/sections/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="main-header">
+            <div className="ed-grid s-grid-5 lg-grid-4">
+                <div className="s-cols-4 lg-cols-1 s-cross-center">
+                    <a href="/">
+                        <img className="main-logo" src="https://media.istockphoto.com/vectors/cupcake-icon-vector-id664864498?b=1&k=20&m=664864498&s=612x612&w=0&h=0d_epUW0U-mwnthY4orp9C_o_LwTgSIL9Qg9hV5eVoY=" alt="Logo EDcupcake" />
+                    </a>
+                </div>
+                <div class="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links">
+          <nav className="main-menu" id="main-menu">
+            <ul>
+              <li><a href="https://www.starwars.com" target="_blank">Inicio</a></li>
+            </ul>
+          </nav>
+          <div class="main-menu-toggle to-l" id="main-menu-toggle"></div>
+        </div>
+      </div>
+
+            
+
+
+
+        </header>
+    )
+}
+
+export default Header
