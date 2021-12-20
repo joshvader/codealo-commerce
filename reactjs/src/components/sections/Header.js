0d11ae7e844c81a2e7c0532ea2c8328505b1605f
proyect_icaro <josuevalenzuela87@gmail.com>
2021-12-20 10:26:19 -0300

    Update

--- reactjs/src/components/sections/Header.js
+++ reactjs/src/components/sections/Header.js
@@ -9,8 +9,8 @@ const Header = () => {
                         <img className="main-logo" src="https://media.istockphoto.com/vectors/cupcake-icon-vector-id664864498?b=1&k=20&m=664864498&s=612x612&w=0&h=0d_epUW0U-mwnthY4orp9C_o_LwTgSIL9Qg9hV5eVoY=" alt="Logo EDcupcake" />
                     </a>
                 </div>
-                <div class="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links">
-          <nav className="main-menu " id="main-menu">
+                <div class="s-grid-1 lg-cols-3 s-cross-center s-main-end header-links white-color">
+          <nav className="main-menu" id="main-menu">
             <ul>
               <li ><a href="https://www.starwars.com" target="_blank">Inicio</a></li>
             </ul>
