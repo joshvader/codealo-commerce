94a17c8d8d6b8ece0f2104d696d1c8715dd67531
proyect_icaro <josuevalenzuela87@gmail.com>
2022-01-06 14:40:48 -0300

    Cambios de ruteo en App.js

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,20 @@
+import React from "react";
 import Home from "./components/sections/Home"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Detalle from "./components/pages/detalle";
 
 import "./styles/styles.css";
 const App = () => {
   return (
-    <div>
-      <Home/>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+      <Routes>
+      
+      <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/detalle" element={<Detalle />}></Route>
+    </Routes>
+    </BrowserRouter>
+    </React.Fragment>
   )
 }
 
